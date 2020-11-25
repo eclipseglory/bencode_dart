cc8c14356bac3c4360538500c2fcaa189e35772f
Dart <947734830@qq.com>
2020-11-25 12:25:02 +0800

    change package name

--- example/bencode_example.dart
+++ example/bencode_example.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:bencode/bencode.dart' as bencode;
+import 'package:bencode_dart/bencode.dart' as bencode;
 
 void main() {
   print(String.fromCharCodes(bencode.encode('string'))); // => "6:string"
